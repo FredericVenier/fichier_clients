76b71c8ff47e17f0b71569415f317e2d5a37c95a
VENIER Frédéric <frederic.venier@telecomnancy.net>
2019-06-06 21:52:54 +0200

    searching bar working

--- src/Controleur/ViewSearch.java
+++ src/Controleur/ViewSearch.java
@@ -5,9 +5,11 @@ import Model.Client;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ViewSearch {
@@ -15,12 +17,34 @@ public class ViewSearch {
 
     @FXML
     private VBox vbox;
+    @FXML
+    private TextField firstname;
+    @FXML
+    private TextField lastname;
 
     public ViewSearch(List<Client> clients) {
         this.clients = clients;
     }
 
     public void init() {
+        updateClientsDisplayed(this.clients);
+    }
+
+    public void newClient() {
+        MenuViews.setViewNewClient();
+    }
+
+    public void seeClient(Client client) {
+        MenuViews.setViewClient(client);
+    }
+
+    public void clickOnSearch() {
+        updateClientsDisplayed(search());
+    }
+
+    private void updateClientsDisplayed(List<Client> clients) {
+        vbox.getChildren().remove(0,vbox.getChildren().size());
+
         for(Client c : clients) {
             HBox hbox = new HBox();
 
@@ -43,11 +67,61 @@ public class ViewSearch {
         }
     }
 
-    public void newClient() {
-        MenuViews.setViewNewClient();
+    private List<Client> search() {
+        String strFirstname = firstname.getText();
+        String strLastname = lastname.getText();
+
+        List<Client> searchingResult = recursive_search(strLastname, clients);
+
+        if(!strFirstname.equals("")) {
+            List<Client> tmp = new ArrayList<>(searchingResult);
+            for(Client c : tmp) {
+                if(!c.getFirstname().startsWith(strFirstname)) {
+                    searchingResult.remove(c);
+                }
+            }
+        }
+
+        return searchingResult;
     }
 
-    public void seeClient(Client client) {
-        MenuViews.setViewClient(client);
+    private List<Client> recursive_search(String lastname, List<Client> clients) {
+        List<Client> searchingResult = new ArrayList<>();
+
+        if(clients.size() == 0 || lastname.equals("")) {
+            return clients;
+        }
+
+        int i = clients.size()/2;
+
+        if(clients.get(i).getLastname().startsWith(lastname)) {
+            searchingResult.add(clients.get(i));
+
+            int j = i-1;
+            if(j>=0) {
+                while (j >= 0 && clients.get(j).getLastname().startsWith(lastname)) {
+                    searchingResult.add(0, clients.get(j));
+                    j = j - 1;
+                }
+            }
+
+            j = i+1;
+            if(j<clients.size()) {
+                while (j < clients.size() && clients.get(j).getLastname().startsWith(lastname)) {
+                    searchingResult.add(clients.get(j));
+                    j = j + 1;
+                }
+            }
+
+            return searchingResult;
+
+        } else if(clients.get(i).getLastname().compareToIgnoreCase(lastname)>0) {
+            return recursive_search(lastname, clients.subList(0,i));
+
+        } else if(i<clients.size()-1) {
+            return recursive_search(lastname, clients.subList(i+1,clients.size()));
+        }
+
+        return searchingResult;
     }
 }
